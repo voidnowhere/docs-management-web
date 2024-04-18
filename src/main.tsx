c196ecf08713be47e28cace865f42e8dcfdebb91
voidnowhere <alivnw@outlook.com>
2024-04-18 14:44:53 +0100

    feat: auth with keycloak

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {AuthProvider} from "react-oidc-context";
+import userManager from "@/config/userManager.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <AuthProvider userManager={userManager}>
+            <App/>
+        </AuthProvider>
+    </React.StrictMode>,
 )
